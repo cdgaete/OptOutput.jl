f5b4cce10d9c0640ad183ca2f72cb1ec5b45f3f5
Carlos Gaete <cdgaete@gmail.com>
2024-10-01 17:32:29 +0200

    Operation commit before 'edit_file' 'src/OptOutput.jl'

--- src/OptOutput.jl
+++ src/OptOutput.jl
@@ -1,38 +1,23 @@
 module OptOutput
 
-using JuMP
 using DataStructures
 using DataFrames
 using JSON3
+using CSV
 
 include("parse_mps.jl")
 include("transform_data.jl")
 include("extract_data.jl")
 
-
-function process_optimization_model(model::Model)
-    # Get the MPS string representation of the model
-    io = IOBuffer()
-    write_to_file(model, io, format=MOI.FileFormats.MPS.Model)
-    mps_string = String(take!(io))
-
-    # Solve the model
-    optimize!(model)
-
-    # Get primal and dual solutions
-    primal_solution = value.(all_variables(model))
-    dual_solution = dual.(all_constraints(model, AffExpr, MOI.EqualTo{Float64}))
-
-    # Extract data
+function process_optimization_results(mps_string::String, primal_solution::Vector{Float64}, dual_solution::Vector{Float64})
     variable_results, equation_results = extract_data(mps_string, primal_solution, dual_solution)
 
-    # Transform data
     predefined_values = create_predefined_values(variable_results)
     case_dimensions = create_case_dimensions(variable_results, predefined_values)
     transformed_dict, dim_to_index, index_to_dim = transform_dict(variable_results, predefined_values, case_dimensions)
 
-    # Create DataFrames
     dataframes = create_dataframes(transformed_dict, index_to_dim, case_dimensions)
+    
 
     return dataframes, variable_results, equation_results
 end
@@ -57,8 +42,10 @@ function save_final_results(dataframes, output_dir="output")
     end
 end
 
-export process_optimization_model,
+export process_optimization_results,
        save_intermediate_results,
-       save_final_results
-
-end # module
+       save_final_results,
+       create_predefined_values,
+       create_case_dimensions,
+       transform_dict
+end
